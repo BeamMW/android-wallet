6f505255a8ae8f85f4a26bc30143dadc3c8cfde5
Tatsiana Khvisiuk <tatsiana@beam-mw.com>
2019-01-21 16:52:01 +0300

    added adaptive icon

--- app/src/main/java/com/mw/beam/beamwallet/core/helpers/EntitiesHelper.kt
+++ app/src/main/java/com/mw/beam/beamwallet/core/helpers/EntitiesHelper.kt
@@ -30,14 +30,6 @@ fun Int.convertToString(): String {
     return sb.toString()
 }
 
-inline fun <T> Iterable<T>.sumByLong(selector: (T) -> Long): Long {
-    var sum = 0L
-    for (element in this) {
-        sum += selector(element)
-    }
-    return sum
-}
-
 enum class TxSender(val value: Boolean) {
     SENT(true), RECEIVED(false);
 
