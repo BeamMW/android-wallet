e044682c808c7bb49b39c9a6233eeb3748249ed8
Tatsiana Khvisiuk <tatsiana@beam-mw.com>
2019-03-20 16:03:19 +0300

    some log style fixes

--- app/src/main/java/com/mw/beam/beamwallet/core/entities/Utxo.kt
+++ app/src/main/java/com/mw/beam/beamwallet/core/entities/Utxo.kt
@@ -37,4 +37,9 @@ data class Utxo(private val source: UtxoDTO) : Parcelable {
     val confirmHeight: Long = source.confirmHeight
     val createTxId: String? = source.createTxId
     val spentTxId: String? = source.spentTxId
+
+    override fun toString(): String {
+        return "\n\nUTXO(\n id=$id\n stringId=$stringId\n amount=$amount\n status=${status.name}\n maturity=$maturity\n keyType=${keyType.name}\n confirmHeight=$confirmHeight\n " +
+                "createTxId=$createTxId\n spentTxId=$spentTxId)"
+    }
 }
