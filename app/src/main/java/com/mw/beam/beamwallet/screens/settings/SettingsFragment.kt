2eaffa4df52ff52d84b9d891243a1f22c396f8d5
Miolin <miolin.dios@gmail.com>
2019-06-04 14:21:33 +0300

    #190 fixed enter max port

--- app/src/main/java/com/mw/beam/beamwallet/screens/settings/SettingsFragment.kt
+++ app/src/main/java/com/mw/beam/beamwallet/screens/settings/SettingsFragment.kt
@@ -226,7 +226,7 @@ class SettingsFragment : BaseFragment<SettingsPresenter>(), SettingsContract.Vie
                 object : InputFilter {
                     override fun filter(source: CharSequence, start: Int, end: Int, dest: Spanned, dstart: Int, dend: Int): CharSequence? {
                         if (source.isNotEmpty()) {
-                            val regExp = "^([^:]*):?([1-9][0-9]*)?$".toRegex()
+                            val regExp = "^([^:]*):?([1-9]|[1-8][0-9]|9[0-9]|[1-8][0-9]{2}|9[0-8][0-9]|99[0-9]|[1-8][0-9]{3}|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9]|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?$".toRegex()
                             return if (!regExp.containsMatchIn(dest.toString().substring(0 until dstart) + source + dest.substring(dend until dest.length))) {
                                 ""
                             } else {
