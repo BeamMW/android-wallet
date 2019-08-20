5f988ee4ead3a6196d9fadbb8ca0485a2c245485
Miolin <miolin.dios@gmail.com>
2019-08-20 12:08:59 +0300

    #36 fix connection after restore from local node

--- app/src/main/java/com/mw/beam/beamwallet/screens/welcome_screen/restore_trusted_node/RestoreTrustedNodeRepository.kt
+++ app/src/main/java/com/mw/beam/beamwallet/screens/welcome_screen/restore_trusted_node/RestoreTrustedNodeRepository.kt
@@ -2,11 +2,16 @@ package com.mw.beam.beamwallet.screens.welcome_screen.restore_trusted_node
 
 import com.mw.beam.beamwallet.base_screen.BaseRepository
 import com.mw.beam.beamwallet.core.App
+import com.mw.beam.beamwallet.core.AppConfig
+import com.mw.beam.beamwallet.core.helpers.PreferencesManager
 
 class RestoreTrustedNodeRepository: BaseRepository(), RestoreTrustedNodeContract.Repository {
     override fun connectToNode(address: String) {
         getResult("changeNodeAddress") {
-            App.wallet?.changeNodeAddress(address)
+            AppConfig.NODE_ADDRESS = address
+            App.wallet?.changeNodeAddress(AppConfig.NODE_ADDRESS)
+            PreferencesManager.putBoolean(PreferencesManager.KEY_CONNECT_TO_RANDOM_NODE, false)
+            PreferencesManager.putString(PreferencesManager.KEY_NODE_ADDRESS, address)
         }
     }
 }
