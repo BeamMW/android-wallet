ce8341779b061fe60f52ff9278497fdbec307821
Maxnflaxl | Beam CM <63856008+Maxnflaxl@users.noreply.github.com>
2024-01-01 18:49:55 +0100

    Update AssetManager.kt
    #638

--- app/src/main/java/com/mw/beam/beamwallet/core/AssetManager.kt
+++ app/src/main/java/com/mw/beam/beamwallet/core/AssetManager.kt
@@ -117,7 +117,7 @@ class AssetManager {
         assetBeamX.shortDesc = "BeamX DAO governance token"
         assetBeamX.longDesc = "BEAMX token is a Confidential Asset issued on top of the Beam blockchain with a fixed emission of 100,000,000 units (except for the lender of a \"last resort\" scenario). BEAMX is the governance token for the BeamX DAO, managed by the BeamX DAO Core contract. Holders can earn BeamX tokens by participating in the DAO activities: providing liquidity to the DeFi applications governed by the DAO or participating in the governance process."
         assetBeamX.site = "https://www.beamxdao.org/"
-        assetBeamX.paper = "https://documentation.beam.mw/overview/beamx-tokenomics"
+        assetBeamX.paper = "https://beam.mw/docs/ecosystem/overview/beamx-tokenomics"
         assets.add(assetBeamX)
     }
 
